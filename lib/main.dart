f5d3c009520a9a96d6a3d301be6a5861cb6f2a1b
Santosh KC <santosh@smartmobe.com>
2020-03-16 12:19:49 +0545

    added required files

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,43 @@
-import 'dart:ffi';
+import "dart:io";
+import "package:flutter/material.dart";
+import "package:path_provider/path_provider.dart";
+import "appointmnets/Appointments.dart";
+import "contacts/Contacts.dart";
+import "notes/Notes.dart";
+import "tasks/Tasks.dart";
+import "utils.dart" as utils;
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+void main() {
+  startMeUp() async {
+    Directory docsDir = await getApplicationDocumentsDirectory();
+  }
 
-void main() => runApp(MyApp());
+  startMeUp();
+}
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext inContext) {
-    return MaterialApp(home : Scaffold( body : GridView.count(
-        padding : EdgeInsets.all(4.0),
-        crossAxisCount : 4, childAspectRatio : 1.0, mainAxisSpacing : 4.0, crossAxisSpacing : 4.0, children: [
-      GridTile(child : new FlutterLogo()),
-      GridTile(child : new FlutterLogo()),
-      GridTile(child : new FlutterLogo()),
-      GridTile(child : new FlutterLogo()),
-      GridTile(child : new FlutterLogo()),
-      GridTile(child : new FlutterLogo()),
-      GridTile(child : new FlutterLogo()),
-      GridTile(child : new FlutterLogo()),
-      GridTile(child : new FlutterLogo())
-    ] )
-    )); }
+class FlutterBook extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+     return MaterialApp(
+       home: DefaultTabController(
+         length: 4,
+         child: Scaffold(
+           appBar: AppBar(
+             title: Text("Flutter Book"),
+             bottom: TabBar(
+               tabs: [
+                 Tab(icon: Icon(Icons.date_range),text: "Appointments"),
+                 Tab(icon: Icon(Icons.contacts),text: "Contacts"),
+                 Tab(icon: Icon(Icons.note),text: "Notes"),
+                 Tab(icon: Icon(Icons.assignment_turned_in),text: "Tasks")
+               ]
+               ),
+           ),
+           body: TabBarView(children: [
+             
+           ],),
+           ),
+         ),
+     );
+  }
 }
